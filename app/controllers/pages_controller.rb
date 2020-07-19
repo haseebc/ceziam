016602167bed1fd8b2d2f4b097563105e8f44cb9
haseebc <haseebchaudhary@gmail.com>
2020-07-19 16:10:27 +0200

    glossary and footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
   
-  skip_before_action :authenticate_user!, only: %i[home about]
+  skip_before_action :authenticate_user!, only: %i[home about glossary]
 
   def home; end
 
   def about; end
+
+  def glossary; end
 
 end
