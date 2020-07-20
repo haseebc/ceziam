2b097cc90cc16563edfe0fe1f54446cefc7901aa
haseebc <haseebchaudhary@gmail.com>
2020-07-20 22:22:36 +0200

    healthcheck page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
   
-  skip_before_action :authenticate_user!, only: %i[home about glossary]
+  skip_before_action :authenticate_user!, only: %i[home about glossary healthcheck]
 
   def home; end
 
   def about; end
 
   def glossary; end
+
+  def healthcheck; end
 
 end
